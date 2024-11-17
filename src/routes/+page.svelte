77391fb7df221a0f04045670b045201454e3b189
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-11-17 21:07:20 +0000

    Basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,40 @@
-<header class="flex justify-center text-5xl py-5 bg-gray-950">
-	<h1 class="font-semibold text-white">Nightbird</h1>
-</header>
+<div class="bg-gray-950">
+	<header class="flex items-center sticky top-0 px-5 py-10 space-x-5 backdrop-blur-sm bg-gray-950/30">
+		<h1 class="text-3xl font-semibold text-white">
+			Nightbird
+		</h1>
+	</header>
 
-<div class="px-10 pt-7">
-	<h1 class="text-xl">
-		Nightbird Game Engine
-	</h1>
+	<main class="h-[260rem] bg-slate-800">
+		<div class="flex items-center px-20 h-[46rem] bg-gradient-to-b from-gray-950 to-slate-800">
+			<div class="space-y-5">
+				<h1 class="text-5xl font-bold text-white">
+					Nightbird Engine
+				</h1>
+				<p class="text-xl text-white">
+					Nightbird is the first AAAA game engine.
+				</p>
+				<div class="flex space-x-3">
+					<div>
+						<a href="https://github.com/NightbirdEngine/Nightbird">
+							<button class="border-2 bg-blue-400 border-blue-400 rounded-xl p-2 text-white hover:bg-gray-700">
+								Download Now
+							</button>
+						</a>
+					</div>
+					<div>
+						<a href="https://github.com/NightbirdEngine/Nightbird">
+							<button class="border-2 rounded-xl p-2 text-white border-white hover:bg-gray-700">
+								Check it out on GitHub
+							</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<footer class="flex justify-center py-5 bg-gray-950">
+		<h1 class="text-white">&copy; 2024 Nightbird Engine</h1>
+	</footer>
 </div>
