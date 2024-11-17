1ab1621857903cdb477b4c3e91495d084b344395
Nightbird <NightbirdEngine@outlook.com>
2024-11-17 23:44:41 +0000

    Improved formatting with different window sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,35 @@
 <div class="bg-gray-950">
-	<header class="flex items-center sticky top-0 px-5 py-10 space-x-5 backdrop-blur-sm bg-gray-950/30">
+	<header class="flex items-center sticky top-0 px-5 py-10 space-x-5 backdrop-blur-sm bg-gray-950/70">
 		<h1 class="text-3xl font-semibold text-white">
 			Nightbird
 		</h1>
 	</header>
 
 	<main class="h-[260rem] bg-slate-800">
-		<div class="flex items-center justify-center md:justify-normal md:px-20 h-[34rem] bg-gradient-to-b from-gray-950 to-slate-800">
-			<div class="space-y-5">
-				<h1 class="text-5xl font-bold text-white">
-					Nightbird Engine
-				</h1>
-				<p class="text-xl text-white">
-					Nightbird is the first AAAA game engine.
-				</p>
-				<div class="flex space-x-3">
-					<div>
-						<a href="https://github.com/NightbirdEngine/Nightbird/releases">
-							<button class="border-2 bg-blue-400 border-blue-400 rounded-xl p-2 text-white hover:bg-gray-700">
-								Download Now
-							</button>
-						</a>
-					</div>
-					<div>
-						<a href="https://github.com/NightbirdEngine/Nightbird">
-							<button class="border-2 rounded-xl p-2 text-white border-white hover:bg-gray-700">
-								Check it out on GitHub
-							</button>
-						</a>
+		<div class="flex justify-center h-[34rem] 2xl:px-14 bg-gradient-to-b from-gray-950 to-slate-800">
+			<div class="flex flex-grow max-w-[110rem]">
+				<div class="flex flex-col flex-grow space-y-5 pt-32 pl-6 sm:pl-8 md:pl-12 lg:pl-14 xl:pl-14 2xl:pl-0">
+					<h1 class="text-4xl md:text-5xl xl:text-6xl font-bold text-white">
+						Nightbird Engine
+					</h1>
+					<p class="text-xl md:text-2xl text-white">
+						Nightbird is the first AAAA game engine.
+					</p>
+					<div class="flex space-x-3">
+						<div>
+							<a href="https://github.com/NightbirdEngine/Nightbird/releases">
+								<button class="border-2 bg-blue-400 border-blue-400 rounded-lg px-3 py-2 text-white hover:bg-gray-700">
+									Download Now
+								</button>
+							</a>
+						</div>
+						<div>
+							<a href="https://github.com/NightbirdEngine/Nightbird">
+								<button class="border-2 rounded-lg px-3 py-2 text-white border-white hover:bg-gray-700">
+									Check it out on GitHub
+								</button>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
