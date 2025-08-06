ef5e92cfc5cf942434d55e5a5dcde701a2d4bc7b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-08-06 13:19:35 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,9 @@
 <div class="flex justify-center h-[34rem] 2xl:px-14">
 	<div class="flex flex-grow max-w-[110rem]">
 		<div class="flex flex-col flex-grow space-y-5 pt-32 pl-6 sm:pl-8 md:pl-12 lg:pl-14 xl:pl-14 2xl:pl-0">
-			<div class="flex flec-grow items-center space-x-1">
-				<img src="/logo-white.svg" alt="Nightbird Logo" class="w-24 h-24" />
-				<h1 class="text-4xl md:text-5xl xl:text-6xl font-bold text-white">
-					Nightbird Engine
-				</h1>
-			</div>
+			<h1 class="text-4xl md:text-5xl xl:text-6xl font-bold text-white">
+				Nightbird Engine
+			</h1>
 		<p class="text-xl md:text-2xl text-white">
 				Nightbird is the first AAAA game engine.
 			</p>
