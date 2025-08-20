70f1f48b005f7816df934137416f9e2eeee05f7b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-08-20 12:17:30 +0100

    Re-added fancy blob with new background method

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,9 +18,19 @@
 			opacity: 0.2;
 		}
 	}
+	.animate-pulse-strong {
+		animation: pulse-strong 5s ease-in-out infinite;
+	}
 </style>
 
-<div class="flex flex-col min-h-screen bg-slate-300 dark:bg-slate-900">
+<div class="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
+	<div
+		class="absolute w-[60vw] h-[60vw] bg-gradient-to-r from-purple-600 via-pink-500 to-purple-500 rounded-full blur-3xl animate-pulse-strong"
+		style="top: 10%; left: 20%;">
+	</div>
+</div>
+
+<div class="flex flex-col min-h-screen">
 	<header class="flex items-center sticky top-0 px-5 py-6 space-x-5 backdrop-blur-lg bg-slate-50/70 dark:bg-slate-950/70">
 		<a href="/" class="flex items-center space-x-1 text-3xl font-semibold text-black dark:text-white">
 			<img src="/logo-white.svg" alt="Nightbird Logo" class="w-16 h-16 hidden dark:block" />
