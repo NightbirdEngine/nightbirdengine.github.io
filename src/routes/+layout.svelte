0a78a371e0da80c8bf60bb81c3545b37635a84e7
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-08-20 12:04:19 +0100

    Removed fancy background for testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,30 +10,17 @@
 </svelte:head>
 
 <style>
-  @keyframes pulse-strong {
-    0%, 100% {
-      opacity: 0.1;
-    }
-    50% {
-      opacity: 0.2;
-    }
-  }
-
-  .animate-pulse-strong {
-    animation: pulse-strong 5s ease-in-out infinite;
-  }
+	@keyframes pulse-strong {
+		0%, 100% {
+			opacity: 0.1;
+		}
+		50% {
+			opacity: 0.2;
+		}
+	}
 </style>
 
-<div class="fixed inset-0 -z-20 bg-slate-300 dark:bg-slate-900">
-	<div
-		class="absolute w-[60vw] h-[60vw] bg-gradient-to-r from-purple-600 via-pink-500 to-purple-500 rounded-full blur-3xl animate-pulse-strong"
-		style="top: 10%; left: 20%;">
-	</div>
-</div>
-
-<div class="absolute top-0 left-0 w-full -z-10 min-h-screen"></div>
-
-<div class="flex flex-col min-h-[100dvh]">
+<div class="flex flex-col min-h-screen bg-slate-300 dark:bg-slate-900">
 	<header class="flex items-center sticky top-0 px-5 py-6 space-x-5 backdrop-blur-lg bg-slate-50/70 dark:bg-slate-950/70">
 		<a href="/" class="flex items-center space-x-1 text-3xl font-semibold text-black dark:text-white">
 			<img src="/logo-white.svg" alt="Nightbird Logo" class="w-16 h-16 hidden dark:block" />
