35cd22e4d9f7c0a30b6895caf734a42e67c5154c
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2026-05-02 00:00:42 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 		Nightbird engine
 	</h1>
 	<p class="text-xl md:text-2xl text-black dark:text-white">
-		Nightbird is a new cross-platform Free and Open Source game engine for Windows, Linux, Wii U and 3DS.
+		Nightbird is a new cross-platform free and open-source game engine for Windows, Linux, Wii U and 3DS.
 	</p>
 	<div class="py-5">
 		<a href="https://github.com/NightbirdEngine/Nightbird" class="cursor-pointer rounded-lg px-4 py-3 text-lg text-white bg-pink-700 hover:bg-pink-600">
